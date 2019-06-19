163690306bab7e62d0f524bf00e5260ababe7093
Javier García <jgarcia@ubicacuenca.com>
2019-06-19 16:11:22 -0500

    deploy

---
+++ precache-manifest.30cb666909d28ceef137a2a5854588cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cz6_diabetest1/robots.txt"
+  },
+  {
+    "revision": "6566592f357ecf21ad2e",
+    "url": "/cz6_diabetest1/js/chunk-vendors.066bf106.js"
+  },
+  {
+    "revision": "82f8bc50d1e8e2a8b8d9",
+    "url": "/cz6_diabetest1/js/app.4b0b0f15.js"
+  },
+  {
+    "revision": "d26864e52def8d120e361c0aeb1f6057",
+    "url": "/cz6_diabetest1/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/cz6_diabetest1/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "6566592f357ecf21ad2e",
+    "url": "/cz6_diabetest1/css/chunk-vendors.bc16c85b.css"
+  },
+  {
+    "revision": "82f8bc50d1e8e2a8b8d9",
+    "url": "/cz6_diabetest1/css/app.8798c4ab.css"
+  }
+];
